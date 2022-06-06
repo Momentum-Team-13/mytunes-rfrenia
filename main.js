85f159840f37f5fc102348f3ac9799ed72338250
rfrenia <ryanfrenia@gmail.com>
2022-06-06 16:21:43 -0400

    mytunes search and display

--- main.js
+++ main.js
@@ -2,14 +2,20 @@
 const search = document.querySelector('#searchMusic')
 const display = document.querySelector('#display')
 const searchButton = document.querySelector('#searchButton')
+const pageElement = document.querySelector('#page')
 
 function musicLog (itunesData) {
     console.log(itunesData)
-    for (let term of itunesData) {
-console.log(term)
-display.textContent = `"test" ${itunesData.results.kind}`
-    }
 
+    itunesData.map (function(song) {
+        console.log ("Songs Line 22", song.trackName)
+
+        let songElement = document.createElement('div')
+        songElement.classList.add('result')
+        songElement.innerText = `${song.artistName} - ${song.trackName}`
+        pageElement.appendChild(songElement)
+       
+    })
 }
 
 searchButton.addEventListener('click', (event) => {
@@ -21,7 +27,6 @@ fetch(exchangeUrl, {
     method: 'GET',
     headers: {'Content-Type': 'application/json' }
 })
-
 .then(function (response) {
     return response.json()
 })
@@ -29,7 +34,5 @@ fetch(exchangeUrl, {
     console.log("Response from exchange API: ", data)
     musicLog(data.results)
 })
-//display.textContent = `"test" ${data.results[0]}`
-
-
 });
+
