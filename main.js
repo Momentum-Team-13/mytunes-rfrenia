9f2bbc157c3070b64edbebf4a0b1a38284a18e71
rfrenia <ryanfrenia@gmail.com>
2022-06-07 11:37:28 -0400

    mytunes search and display

--- main.js
+++ main.js
@@ -4,17 +4,33 @@ const display = document.querySelector('#display')
 const searchButton = document.querySelector('#searchButton')
 const pageElement = document.querySelector('#page')
 
+
+
 function musicLog (itunesData) {
     console.log(itunesData)
 
+
     itunesData.map (function(song) {
-        console.log ("Songs Line 22", song.trackName)
+        console.log ("Songs Line 11", song.trackName)
 
         let songElement = document.createElement('div')
         songElement.classList.add('result')
-        songElement.innerText = `${song.artistName} - ${song.trackName}`
+        songElement.innerText = `${song.artistName} - ${song.trackName} `
         pageElement.appendChild(songElement)
-       
+
+        let audioElement = document.createElement('audio')
+        audioElement.src = song.previewUrl
+        audioElement.alt = 'play song'
+        audioElement.classList.add("audio")
+        pageElement.appendChild(audioElement)
+        console.log ("audio Line 26", song.previewUrl)
+
+        let imageElement = document.createElement('img')
+        imageElement.src = song.artworkUrl100
+        imageElement.alt = 'photos of artist'
+        imageElement.classList.add("img")
+        pageElement.appendChild(imageElement)
+
     })
 }
 
@@ -36,3 +52,29 @@ fetch(exchangeUrl, {
 })
 });
 
+document.addEventListener('keyup', (event) => {
+  if (event.key === "Enter") {
+    console.log(search.value)
+    const userInput = search.value
+    let exchangeUrl = `https://itunes.apple.com/search?term=${userInput}`;
+    
+    fetch(exchangeUrl, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json' }
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        console.log("Response from exchange API: ", data)
+        musicLog(data.results)
+    }) 
+    }
+});
+
+
+function clearingDisplay(){
+    displayText = '';
+    operator = '';
+    updateDisplayText();
+  }
