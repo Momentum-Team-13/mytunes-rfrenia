76ea06035ed4e1be6a758ade3144b17fd09e7755
rfrenia <ryanfrenia@gmail.com>
2022-06-08 12:44:10 -0400

    clean up

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 const pageElement = document.querySelector('#page')
 const search = document.querySelector('#searchMusic')
 const noResults = document.querySelector('#display')
-// pageElement.appendChild(displayElement)
 const searchButton = document.querySelector('#searchButton')
 const audioElement = document.querySelector('audio')
 const imageDisplay = document.querySelector('#imgHeader')
 const textHeader = document.querySelector('#textHeader')
 
 
-window.scroll({
-    top:0,
-    left: 0,
-    behavior:'smooth'
-});
-
 function musicLog (itunesData) {
     console.log(itunesData)
     const displayElement = document.querySelector('#display')
     
- displayElement.innerHTML = ''
+    //clear results for new searches
+    displayElement.innerHTML = ''
+
     itunesData.map (function(song) {
         console.log ("Songs Line 16", song.trackName)
 
         let songElement = document.createElement('div')
         songElement.classList.add('result')
 
+        //display artist name and song name from search
         let textElement = document.createElement('div')
         textElement.innerText = `${song.artistName} - "${song.trackName}" `
         songElement.appendChild(textElement)
 
+        //display artwork from search
         let imageElement = document.createElement('img')
         imageElement.src = song.artworkUrl100
         imageElement.alt = 'photos of artist'
@@ -38,6 +35,7 @@ function musicLog (itunesData) {
 
         displayElement.appendChild(songElement)
 
+        //play music when artwork is clicked
         imageElement.addEventListener('click', (event) => {
         if (audioElement.src !== song.previewUrl) {
             imageDisplay.src = song.artworkUrl100
@@ -45,50 +43,50 @@ function musicLog (itunesData) {
             audioElement.controls = true
             textHeader.innerText = `${song.artistName} - ${song.trackName}`
         }
+
+        //pause audio when artwork is clicked
         if (audioElement.paused) {
         audioElement.play()
-        }else {
-            audioElement.pause()
+        } else {
+        audioElement.pause()
         }
         })
-        })
-        
+    })
+}
 
-        
-    }
-
-
-
+//use the mouse click key to submit search
 searchButton.addEventListener('click', (event) => {
 console.log(search.value)
 const userInput = search.value
 let exchangeUrl = `https://itunes.apple.com/search?term=${userInput}`;
 
-fetch(exchangeUrl, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json' }
-})
-.then(function (response) {
-    return response.json()
-})
-.then(function (data){
+    fetch(exchangeUrl, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json' }
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
     console.log("Response from exchange API: ", data)
     musicLog(data.results)
-    if ( data.results < 1) {
-        let noResultsElement = document.createElement('div')
-        noResults.innerText = "No Results"
-        console.log(noResultsElement.innerText)
-        
-    }
-})
-.catch(err => {
-    window.alert("search error")
-    console.error(err);
-})
+
+        if ( data.results < 1) {
+            let noResultsElement = document.createElement('div')
+            noResults.innerText = "No Results"
+            console.log(noResultsElement.innerText)
+        } 
+    })
+
+    .catch(err => {
+        window.alert("search error")
+        console.error(err);
+    })
 });
 
+//use the "enter" key to submit search
 document.addEventListener('keyup', (event) => {
-  if (event.key === "Enter") {
+    if (event.key === "Enter") {
     console.log(search.value)
     const userInput = search.value
     let exchangeUrl = `https://itunes.apple.com/search?term=${userInput}`;
@@ -107,10 +105,11 @@ document.addEventListener('keyup', (event) => {
             let noResultsElement = document.createElement('div')
             noResults.innerText = "No Results"
             console.log(noResultsElement.innerText)
-    }
-}) 
+        }
+    }) 
     .catch(err => {
         window.alert("search error")
         console.error(err);
+    })
+    }
 })
-  }})
